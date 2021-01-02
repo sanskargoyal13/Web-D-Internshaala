a37c6ee21c5661fcb555b638193eb6bb4aef6a18
sanskargoyal13 <sanskargoyal1317@gmail.com>
2021-01-03 02:39:29 +0530

    Adding remaining Files

---
+++ header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-primary">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="#">Friends</a>
+		</div>
+		<div>
+			<ul class="nav navbar-nav navbar-right">
+  				<li><a href="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+  				<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login </a></li>
+  			</ul>
+  		</div>
+  	</div>
+</nav>
